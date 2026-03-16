7c6ac594244269a799927a940f909cf6e8d8a837
rayhanaznrambe <rayhanaznrambe@gmail.com>
2026-03-16 10:09:41 +0700

    Update books.js

--- books.js
+++ books.js
@@ -3,211 +3,202 @@ const books = [
 /* ================= NOVEL ================= */
 
 {
-judul:"Pride and Prejudice",
-penulis:"Jane Austen",
-tahun:"1813",
-kategori:"novel",
-cover:"https://covers.openlibrary.org/b/isbn/9780141439518-L.jpg",
-file:"https://www.gutenberg.org/ebooks/1342"
+    judul: "Pride and Prejudice",
+    penulis: "Jane Austen",
+    tahun: "1813",
+    kategori: "novel",
+    cover: "https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/1342"
 },
 
 {
-judul:"Frankenstein",
-penulis:"Mary Shelley",
-tahun:"1818",
-kategori:"novel",
-cover:"https://covers.openlibrary.org/b/isbn/9780486282114-L.jpg",
-file:"https://www.gutenberg.org/ebooks/84"
+    judul: "Frankenstein",
+    penulis: "Mary Shelley",
+    tahun: "1818",
+    kategori: "novel",
+    cover: "https://www.gutenberg.org/cache/epub/84/pg84.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/84"
 },
 
 {
-judul:"Dracula",
-penulis:"Bram Stoker",
-tahun:"1897",
-kategori:"novel",
-cover:"https://covers.openlibrary.org/b/isbn/9780486411095-L.jpg",
-file:"https://www.gutenberg.org/ebooks/345"
+    judul: "Dracula",
+    penulis: "Bram Stoker",
+    tahun: "1897",
+    kategori: "novel",
+    cover: "https://www.gutenberg.org/cache/epub/345/pg345.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/345"
 },
 
 {
-judul:"Alice's Adventures in Wonderland",
-penulis:"Lewis Carroll",
-tahun:"1865",
-kategori:"novel",
-cover:"https://covers.openlibrary.org/b/isbn/9780486275437-L.jpg",
-file:"https://www.gutenberg.org/ebooks/11"
+    judul: "Alice's Adventures in Wonderland",
+    penulis: "Lewis Carroll",
+    tahun: "1865",
+    kategori: "novel",
+    cover: "https://www.gutenberg.org/cache/epub/11/pg11.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/11"
 },
 
 {
-judul:"The Adventures of Sherlock Holmes",
-penulis:"Arthur Conan Doyle",
-tahun:"1892",
-kategori:"novel",
-cover:"https://covers.openlibrary.org/b/isbn/9780486474915-L.jpg",
-file:"https://www.gutenberg.org/ebooks/1661"
+    judul: "The Adventures of Sherlock Holmes",
+    penulis: "Arthur Conan Doyle",
+    tahun: "1892",
+    kategori: "novel",
+    cover: "https://www.gutenberg.org/cache/epub/1661/pg1661.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/1661"
 },
 
 {
-judul:"Moby Dick",
-penulis:"Herman Melville",
-tahun:"1851",
-kategori:"novel",
-cover:"https://covers.openlibrary.org/b/isbn/9781503280786-L.jpg",
-file:"https://www.gutenberg.org/ebooks/2701"
+    judul: "Moby Dick",
+    penulis: "Herman Melville",
+    tahun: "1851",
+    kategori: "novel",
+    cover: "https://www.gutenberg.org/cache/epub/2701/pg2701.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/2701"
 },
 
 /* ================= AKADEMIK ================= */
 
 {
-judul:"On the Origin of Species",
-penulis:"Charles Darwin",
-tahun:"1859",
-kategori:"akademik",
-cover:"https://covers.openlibrary.org/b/isbn/9781509827695-L.jpg",
-file:"https://www.gutenberg.org/ebooks/1228"
+    judul: "On the Origin of Species",
+    penulis: "Charles Darwin",
+    tahun: "1859",
+    kategori: "akademik",
+    cover: "https://books.google.com/books/content?id=vS9VAAAAYAAJ&printsec=frontcover&img=1&zoom=1",
+    file: "https://www.gutenberg.org/ebooks/1228"
 },
 
 {
-judul:"Relativity: The Special and General Theory",
-penulis:"Albert Einstein",
-tahun:"1916",
-kategori:"akademik",
-cover:"https://covers.openlibrary.org/b/isbn/9780486417141-L.jpg",
-file:"https://www.gutenberg.org/ebooks/30155"
+    judul: "Relativity: The Special and General Theory",
+    penulis: "Albert Einstein",
+    tahun: "1916",
+    kategori: "akademik",
+    cover: "https://books.google.com/books/content?id=5uYpAAAAYAAJ&printsec=frontcover&img=1&zoom=1",
+    file: "https://www.gutenberg.org/ebooks/30155"
 },
 
 {
-judul:"The Principles of Psychology",
-penulis:"William James",
-tahun:"1890",
-kategori:"akademik",
-cover:"https://covers.openlibrary.org/b/isbn/9780486203810-L.jpg",
-file:"https://www.gutenberg.org/ebooks/57628"
+    judul: "The Principles of Psychology",
+    penulis: "William James",
+    tahun: "1890",
+    kategori: "akademik",
+    cover: "https://books.google.com/books/content?id=Yp_OAAAAMAAJ&printsec=frontcover&img=1&zoom=1",
+    file: "https://www.gutenberg.org/ebooks/57628"
 },
 
 {
-judul:"The Elements of Euclid",
-penulis:"Euclid",
-tahun:"300 BC",
-kategori:"akademik",
-cover:"https://covers.openlibrary.org/b/isbn/9780486600886-L.jpg",
-file:"https://www.gutenberg.org/ebooks/21076"
+    judul: "The Elements of Euclid",
+    penulis: "Euclid",
+    tahun: "300 BC",
+    kategori: "akademik",
+    cover: "https://books.google.com/books/content?id=66NCAAAAYAAJ&printsec=frontcover&img=1&zoom=1",
+    file: "https://www.gutenberg.org/ebooks/21076"
 },
 
 /* ================= FILSAFAT ================= */
 
 {
-judul:"Meditations",
-penulis:"Marcus Aurelius",
-tahun:"180",
-kategori:"filsafat",
-cover:"https://covers.openlibrary.org/b/isbn/9780140449334-L.jpg",
-file:"https://www.gutenberg.org/ebooks/2680"
+    judul: "Meditations",
+    penulis: "Marcus Aurelius",
+    tahun: "180",
+    kategori: "filsafat",
+    cover: "https://www.gutenberg.org/cache/epub/2680/pg2680.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/2680"
 },
 
 {
-judul:"The Art of War",
-penulis:"Sun Tzu",
-tahun:"500 BC",
-kategori:"filsafat",
-cover:"https://covers.openlibrary.org/b/isbn/9781599869773-L.jpg",
-file:"https://www.gutenberg.org/ebooks/132"
+    judul: "The Art of War",
+    penulis: "Sun Tzu",
+    tahun: "500 BC",
+    kategori: "filsafat",
+    cover: "https://books.google.com/books/content?id=vS9VAAAAYAAJ&printsec=frontcover&img=1&zoom=1",
+    file: "https://www.gutenberg.org/ebooks/132"
 },
 
 {
-judul:"Thus Spoke Zarathustra",
-penulis:"Friedrich Nietzsche",
-tahun:"1883",
-kategori:"filsafat",
-cover:"https://covers.openlibrary.org/b/isbn/9780140441185-L.jpg",
-file:"https://www.gutenberg.org/ebooks/1998"
+    judul: "Thus Spoke Zarathustra",
+    penulis: "Friedrich Nietzsche",
+    tahun: "1883",
+    kategori: "filsafat",
+    cover: "https://www.gutenberg.org/cache/epub/1998/pg1998.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/1998"
 },
 
 /* ================= SEJARAH ================= */
 
 {
-judul:"The Histories",
-penulis:"Herodotus",
-tahun:"440 BC",
-kategori:"sejarah",
-cover:"https://covers.openlibrary.org/b/isbn/9780140449082-L.jpg",
-file:"https://www.gutenberg.org/ebooks/2707"
+    judul: "The Histories",
+    penulis: "Herodotus",
+    tahun: "440 BC",
+    kategori: "sejarah",
+    cover: "https://www.gutenberg.org/cache/epub/2707/pg2707.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/2707"
 },
 
 {
-judul:"The History of the Peloponnesian War",
-penulis:"Thucydides",
-tahun:"431 BC",
-kategori:"sejarah",
-cover:"https://covers.openlibrary.org/b/isbn/9780140440393-L.jpg",
-file:"https://www.gutenberg.org/ebooks/7142"
+    judul: "The History of the Peloponnesian War",
+    penulis: "Thucydides",
+    tahun: "431 BC",
+    kategori: "sejarah",
+    cover: "https://www.gutenberg.org/cache/epub/7142/pg7142.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/7142"
 },
 
 {
-judul:"The Decline and Fall of the Roman Empire",
-penulis:"Edward Gibbon",
-tahun:"1776",
-kategori:"sejarah",
-cover:"https://covers.openlibrary.org/b/isbn/9780140437645-L.jpg",
-file:"https://www.gutenberg.org/ebooks/25717"
+    judul: "The Decline and Fall of the Roman Empire",
+    penulis: "Edward Gibbon",
+    tahun: "1776",
+    kategori: "sejarah",
+    cover: "https://www.gutenberg.org/cache/epub/25717/pg25717.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/25717"
 },
 
 /* ================= BIOGRAFI ================= */
 
 {
-judul:"The Autobiography of Benjamin Franklin",
-penulis:"Benjamin Franklin",
-tahun:"1791",
-kategori:"biografi",
-cover:"https://covers.openlibrary.org/b/isbn/9780486264660-L.jpg",
-file:"https://www.gutenberg.org/ebooks/20203"
+    judul: "The Autobiography of Benjamin Franklin",
+    penulis: "Benjamin Franklin",
+    tahun: "1791",
+    kategori: "biografi",
+    cover: "https://www.gutenberg.org/cache/epub/20203/pg20203.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/20203"
 },
 
 {
-judul:"Life of Napoleon Bonaparte",
-penulis:"William Milligan Sloane",
-tahun:"1896",
-kategori:"biografi",
-cover:"https://covers.openlibrary.org/b/isbn/9781438500973-L.jpg",
-file:"https://www.gutenberg.org/ebooks/7170"
+    judul: "Life of Napoleon Bonaparte",
+    penulis: "William Milligan Sloane",
+    tahun: "1896",
+    kategori: "biografi",
+    cover: "https://www.gutenberg.org/cache/epub/7170/pg7170.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/7170"
 },
 
 {
-judul:"The Life of Samuel Johnson",
-penulis:"James Boswell",
-tahun:"1791",
-kategori:"biografi",
-cover:"https://covers.openlibrary.org/b/isbn/9780140436624-L.jpg",
-file:"https://www.gutenberg.org/ebooks/1564"
+    judul: "The Life of Samuel Johnson",
+    penulis: "James Boswell",
+    tahun: "1791",
+    kategori: "biografi",
+    cover: "https://www.gutenberg.org/cache/epub/1564/pg1564.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/1564"
 },
 
 /* ================= JURNAL / RESEARCH ================= */
 
 {
-judul:"Experimental Researches in Electricity",
-penulis:"Michael Faraday",
-tahun:"1839",
-kategori:"jurnal",
-cover:"https://covers.openlibrary.org/b/isbn/9780486443430-L.jpg",
-file:"https://www.gutenberg.org/ebooks/14985"
+    judul: "Experimental Researches in Electricity",
+    penulis: "Michael Faraday",
+    tahun: "1839",
+    kategori: "jurnal",
+    cover: "https://www.gutenberg.org/cache/epub/14985/pg14985.cover.medium.jpg",
+    file: "https://www.gutenberg.org/ebooks/14985"
 },
 
 {
-judul:"The Structure of Scientific Revolutions",
-penulis:"Thomas Kuhn",
-tahun:"1962",
-kategori:"jurnal",
-cover:"https://covers.openlibrary.org/b/isbn/9780226458144-L.jpg",
-file:"https://archive.org/details/structureofscien0000kuhn"
-},
-
-{
-judul:"The Principles of Economics",
-penulis:"Alfred Marshall",
-tahun:"1890",
-kategori:"jurnal",
-cover:"https://covers.openlibrary.org/b/isbn/9781605200347-L.jpg",
-file:"https://www.gutenberg.org/ebooks/43561"
+    judul: "The Principles of Economics",
+    penulis: "Alfred Marshall",
+    tahun: "1890",
+    kategori: "jurnal",
+    cover: "https://books.google.com/books/content?id=j098AAAAMAAJ&printsec=frontcover&img=1&zoom=1",
+    file: "https://www.gutenberg.org/ebooks/43561"
 }
 
 ];
